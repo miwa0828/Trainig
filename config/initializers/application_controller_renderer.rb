65c30b0fa25cd6ee30b2f44dd991f1fd088f81a0
EC2 Default User <ec2-user@ip-172-31-30-62.ap-northeast-1.compute.internal>
2021-08-11 05:54:43 +0000

    機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '6a91c38cfbc0b5d91f7677c2e4848dd2c4a72a9671b7e5587b9eea9eceff2ac45849dc533f890f6451c969f041ad31e3fdc5099a1784483f9c35fbb623406ff2'
